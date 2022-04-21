a58f696ff527bf8ccc66c9a2a2610745afdc27d5
Rick Ansay <rick.ansay@gmail.com>
2022-04-21 17:32:41 -0600

    Create get requests in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,40 @@
 const { User } = require('../models');
 
 module.exports = {
-    createUser: async (req, res, next) => {
-        const { username, email } = req.body;
-        
-    }
+    //create user
+    createUser: async (req, res) => {
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
+            return res.status(400).json({ error: 'You must provide a username, email, and password!' });
+        }
+        try {
+            const user = await User.create({
+                username, 
+                email, 
+                password,
+            });
+            res.json(user);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+
+    getAllUsers: async (req, res) => {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+
+    getUserById: async (req, res) => {
+        const { userId } = req.params;
+        try {
+            const user = await User.findById(userId);
+            res.json(user);
+        } catch (e) {
+            res.json(e);
+        }
+    },
 };
